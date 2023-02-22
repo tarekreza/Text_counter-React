c3de5271b43c84cf1d81bd82d3e4a336bc1abcae
Muhammad Tarek Reza <tarekreza15@gmail.com>
2023-02-22 23:50:23 +0600

    text area color change

--- src/App.jsx
+++ src/App.jsx
@@ -40,13 +40,13 @@ function App() {
       {/* <div className="bg-[#111827] h-screen"> */}
       <div className="bg-[url('background.svg')] h-screen">
         <div className="w-4/5 mx-auto">
-          <h1 className="pt-16 text-white lg:text-lg font-serif">Enter your text</h1>
+          <h1 className="pt-16 text-white lg:text-xl font-serif">Enter your text here</h1>
           <textarea
             value={text}
             onChange={handleOnChange}
             id="message"
-            rows="6"
-            className="my-2 mx-auto w-full block p-2.5  text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            rows="8"
+            className="my-2 mx-auto w-full block p-2.5  text-base text-white bg-gray-800 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
             placeholder="Write your text to analyze..."
           />
           <p className="text-white">Total character : {text.length}</p>
